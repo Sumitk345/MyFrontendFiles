e92c194b6b46e80368cbd9e077a865f8e13eb356
Sumitk345 <sumitkolekar345.com>
2024-09-18 19:58:14 +0530

    Logo Added Pharmacy

--- ReactJS/pharmacy-web/src/Index/header.js
+++ ReactJS/pharmacy-web/src/Index/header.js
@@ -2,7 +2,7 @@ let Header = () => {
       return (
         <nav className="navbar bg-success p-5 p-md-4 p-lg-3">
             <div className="container-fluid">
-                <a className="navbar-brand fs-1 fw-bold text-white tracking-wider">Stanley's Pharmacy</a>
+                <a className="navbar-brand fs-1 fw-bold text-white tracking-wider"><i class="fa-regular fa-hospital fs-1" style={{"color": "#ffffff"}}><span className="px-2"></span></i>Stanley's Pharmacy</a>
                 <form className="d-flex" role="search">
                     <div className="d-flex  gap-2">
                         <button type="button" className="btn btn-outline-light">Sign Up</button>
